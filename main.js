e5f2a8a1f2b2c25a2960dd6c41147ef66ff9f7db
bobbycrooz <you@example.com>
2020-09-07 03:53:54 -0700

    updated javascripts function

--- main.js
+++ main.js
@@ -1,23 +1,28 @@
-// main javascript file for cooming-soon-pro=ject
-
-// ToDo:dislay warning-icon and text when email validation is false
-
-// export button to variable
-const btn = document.getElementById('input__submit_btn');
-// grab css display properties for warning-icon and text
-const wIcon = document.getElementById('wIcon');
-const wText = document.querySelector('.content__input');
-// grab user input value
-const emailValue = document.getElementById('email').value;
-
-// function validateEmail(email) {
-//     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-//     return re.test(String(email).toLowerCase());
-// }
-
-// the general email pattern
-const emailPattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    
+var btn = document.getElementById("submit-btn");
+var w_icon = document.getElementById("warning-icon");
+var w_text = document.querySelector(".content__input");
+var formValue = document.getElementById("email");
+var formclass = document.querySelector(".content_email");
 
 
+// const re = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 
+function warning(e){
+        e.preventDefault();
+
+        if (!formValue.validity.valid || !input.value){
+                w_icon.style.display = "block"
+                w_text.classList.add("show")
+                formValue.style.border ="2.5px solid var(--primary_2)"
+
+                // a function to be executed after a time out
+                setTimeout(() => {
+                    w_icon.style.display = "none"
+                    w_text.classList.remove("show")
+                    formValue.style.border ="1px solid var(--primary_1)"
+                },2000);
+        }
+}
+
+btn.addEventListener("click",warning);
+btn.addEventListener("submit",warning);
